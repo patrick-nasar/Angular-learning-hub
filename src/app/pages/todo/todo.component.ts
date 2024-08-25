fff2435a3cef7f9880f4e6ee9b6c902a1b84373d
Patrick <patriknasar@gmail.com>
2024-08-26 00:28:07 +0200

    made todo list and some style changes

--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,12 +1,21 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgFor],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
+
+  todoList: String[] = [];
+
+  saveInput(value: string) {
+    this.todoList.push(value);
+    console.log('Stored value:', this.todoList);
+  }
 
 }
