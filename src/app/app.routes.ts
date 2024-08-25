b838441439b4806b21acbf820b08bd948ccd4b93
Patrick <patriknasar@gmail.com>
2024-08-25 20:09:30 +0200

    On my first day using Angular, I learned about file structure which is completely different from React.js, routing, and how to connect Typescript, HTML, and CSS files with each other

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TodoComponent } from './pages/todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'todo', component: TodoComponent },
+];
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  counter: number = 0
+
+  increase() {
+    this.counter = this.counter + 1
+  }
+}
